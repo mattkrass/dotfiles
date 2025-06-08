5779b201f4bd8a4235aeefa9f81726b27d6d73ec
Matt Krass <mattkrass@gmail.com>
2025-06-07 21:30:00 -0400

    Updated plugins

---
+++ nvim/.config/nvim/lua/plugins/ctrlspace.lua
@@ -0,0 +1,9 @@
+return {
+    "vim-ctrlspace/vim-ctrlspace",
+    priority = 1000,
+    config = function()
+        -- set up key mapping
+        vim.g.CtrlSpaceDefaultMappingKey = "<C-Space> "
+        vim.keymap.set("n", "<C-Space>", "<cmd>CtrlSpace<CR>", { desc = "Open CtrlSpace" })
+    end
+}
