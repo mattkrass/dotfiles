b6f383f8ba824ba9d1787687e8cf5a69ff5e3558
Matt Krass <mattkrass@gmail.com>
2025-06-07 21:32:21 -0400

    Disable transparency in terminal

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,7 +13,4 @@ config.window_decorations = "RESIZE"
 
 config.color_scheme = 'Dracula (Official)'
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
 return config
