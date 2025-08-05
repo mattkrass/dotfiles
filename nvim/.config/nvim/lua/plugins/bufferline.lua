1b72c62442743174a358355a9b109d6d46c1aafd
Matt Krass <mattkrass@gmail.com>
2025-08-04 23:18:50 -0400

    New plugins

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,10 +2,15 @@ return {
     "akinsho/bufferline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     version = "*",
+    lazy = false,
     opts = {
         options = {
             mode = 'tabs',
             separator_style = 'slant',
         }
+    },
+    keys = {
+        -- { "gt", "<cmd>bn<CR>", desc = "Next buffer" },
+        -- { "gT", "<cmd>bp<CR>", desc = "Previous buffer" },
     }
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -11,7 +11,21 @@ return {
         dashboard = { enabled = true },
         explorer = { enabled = true },
         indent = { enabled = true },
-        input = { enabled = true },
+        input = {
+            enabled = true,
+            position = "float",
+            title_pos = "center",
+            relative = "cursor",
+            wo = {
+                winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+                cursorline = false,
+            },
+            bo = {
+                filetype = "snacks_input",
+                buftype = "prompt",
+            },
+        },
+        lazygit = {},
         picker = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
@@ -20,4 +34,28 @@ return {
         statuscolumn = { enabled = true },
         words = { enabled = true },
     },
+    keys = {
+        -- Top Pickers & Explorer
+        { "<leader><space>", function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
+        { "<leader>,",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+        { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
+        { "<leader>:",       function() Snacks.picker.command_history() end,                         desc = "Command History" },
+        { "<leader>n",       function() Snacks.picker.notifications() end,                           desc = "Notification History" },
+        { "<leader>e",       function() Snacks.explorer() end,                                       desc = "File Explorer" },
+        -- find
+        { "<leader>fb",      function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
+        { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+        { "<leader>ff",      function() Snacks.picker.files() end,                                   desc = "Find Files" },
+        { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
+        { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
+        { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
+        -- git
+        { "<leader>gb",      function() Snacks.picker.git_branches() end,                            desc = "Git Branches" },
+        { "<leader>gl",      function() Snacks.picker.git_log() end,                                 desc = "Git Log" },
+        { "<leader>gL",      function() Snacks.picker.git_log_line() end,                            desc = "Git Log Line" },
+        { "<leader>gs",      function() Snacks.picker.git_status() end,                              desc = "Git Status" },
+        { "<leader>gS",      function() Snacks.picker.git_stash() end,                               desc = "Git Stash" },
+        { "<leader>gd",      function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
+        { "<leader>gf",      function() Snacks.picker.git_log_file() end,                            desc = "Git Log File" },
+    }
 }
