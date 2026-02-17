da47fcc9aec71071a26e3c7d81559ef062c72930
Matt Krass <mkrass1@bloomberg.net>
2026-02-17 09:56:10 -0500

    Add CMake shortcuts, normalize branch

--- nvim/.config/nvim/lua/plugins/cmake-tools.lua
+++ nvim/.config/nvim/lua/plugins/cmake-tools.lua
@@ -4,6 +4,13 @@ return {
          { "akinsho/toggleterm.nvim", version = "*", config = true }
     },
     opts = {
-        cmake_kits_path = "/root/.local/share/CMakeTools/cmake-tools-kits.json"
-    }
+        cmake_kits_path = "/root/.local/share/CMakeTools/cmake-tools-kits.json",
+        cmake_build_directory = "/build",
+        cmake_build_options = { "-j20" },
+    },
+    init = function(plugin)
+        vim.keymap.set("n", "<leader>cg", "<cmd>CMakeGenerate<CR>", { desc = "Generate a CMake project" })                     -- open new tab
+        vim.keymap.set("n", "<leader>cb", "<cmd>CMakeBuild<CR>", { desc = "Build a CMake project" })                     -- open new tab
+        vim.keymap.set("n", "<leader>ct", "<cmd>CMakeBuild<CR>", { desc = "Test a CMake project" })                     -- open new tab
+    end,
 }
