56142a1625fc61c97fe5d4d012132bd06f755f13
Matt Krass <mkrass1@bloomberg.net>
2026-02-19 18:55:43 +0000

    Unlock telescope version

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
     "nvim-telescope/telescope.nvim",
-    version = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         require('telescope').setup({
