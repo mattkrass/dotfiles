1cbbde6cc7a88ffc42e298f19afec56c3656f5bc
Matt Krass <mkrass1@bloomberg.net>
2026-03-13 00:45:15 -0400

    Update treesitter/conform

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,55 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        vim.api.nvim_create_autocmd("FileType", {
-          callback = function(ev)
-            if pcall(vim.treesitter.start) then
-              -- if you want ts folds...
-              if vim.treesitter.query.get(ev.match, "folds") then
-                vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                vim.wo[0][0].foldmethod = "expr"
-              end
-            end
-          end,
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		-- Install core parsers after lazy.nvim finishes loading all plugins
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyDone",
+			once = true,
+			callback = function()
+				require("nvim-treesitter").install({
+					"bash",
+					"comment",
+					"diff",
+					"git_config",
+					"git_rebase",
+					"gitcommit",
+					"gitignore",
+					"html",
+					"javascript",
+					"json",
+					"latex",
+					"lua",
+					"luadoc",
+					"make",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+				}, {
+					max_jobs = 8,
+				})
+			end,
+		})
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(ev)
+				if pcall(vim.treesitter.start) then
+					-- if you want ts folds...
+					if vim.treesitter.query.get(ev.match, "folds") then
+						vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+						vim.wo[0][0].foldmethod = "expr"
+					end
+				end
+			end,
+		})
+	end,
 }
